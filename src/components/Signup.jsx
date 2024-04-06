216c69eefe40e696a782ec0cc6dbb31a08b47d7e
Nikhil <nikhilgudadhe63@gmail.com>
2024-04-06 15:34:27 +0530

    Input component added

---
+++ src/components/Signup.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function Signup() {
+  return (
+    <div className="h-screen overflow-y-auto bg-[#121212] text-white">
+    <div className="mx-auto my-8 flex w-full max-w-sm flex-col px-4">
+        
+    </div>
+    </div>
+  )
+}
+
+export default Signup
